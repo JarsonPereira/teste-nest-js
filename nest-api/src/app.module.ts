cd10faede6b32fa5482b514bf780d35b4250abac
Aluisio Pereira da Cruz Júnior <aluisio205@gmail.com>
2021-11-24 16:20:47 -0300

    Recriação do app.module

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,8 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CategoryController } from "./controllers/category.controller";
+import { Category } from "./entity/category.entity";
+import { Product } from "./entity/product.entity";
+import { CategoryRepository } from "./repository/category.repository";
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Category, Product])],
+  controllers: [CategoryController],
+  providers: [CategoryRepository],
 })
+
 export class AppModule { }
