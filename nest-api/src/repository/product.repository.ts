bd4b55cda62aef3b1437cec8f5fc1796dbe0c8d3
Aluisio Pereira da Cruz Júnior <aluisio205@gmail.com>
2021-11-24 14:20:18 -0300

    Identação

--- nest-api/src/repository/product.repository.ts
+++ nest-api/src/repository/product.repository.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { retry } from "rxjs";
 import { Repository, RepositoryNotFoundError } from "typeorm";
 import { Product } from "../entity/product.entity";
 import { ProductContracts } from "./contratcs/productContratc";
@@ -16,17 +15,17 @@ export class ProductRepository implements ProductContracts {
 
     async getById(id: number): Promise<Product> {
         return await this.repository.findOne(id);
-
     }
+
     async post(product: Product) {
         await this.repository.save(product);
     }
+
     async put(id: number, product: Product) {
         await this.repository.update(id, product)
     }
+
     async delete(id: number) {
         await this.repository.delete(id);
     }
-
-
 }
