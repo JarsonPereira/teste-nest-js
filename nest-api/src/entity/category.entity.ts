94304cef9763ca2b986520ddd8f150142e4a5103
Aluisio Pereira da Cruz Júnior <aluisio205@gmail.com>
2021-11-24 09:50:56 -0300

    Criação das Entidades Categoria e Produto

---
+++ nest-api/src/entity/category.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
+import { Product } from "./product.entity";
+
+
+@Entity()
+export class Category {
+
+    @PrimaryColumn()
+    id: number
+
+    @Column("varchar", { length: 30 })
+    name: string
+
+    @OneToMany(() => Product, categoria => Category)
+    products: Product[];
+}
---
+++ nest-api/src/entity/product.entity.ts
@@ -0,0 +1,24 @@
+import { type } from "os";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryColumn } from "typeorm";
+import { Category } from "./category.entity";
+
+
+@Entity()
+export class Product {
+
+    @PrimaryColumn()
+    id: number
+
+    @Column("varchar", { length: 30 })
+    name: string
+
+    @Column({ length: 50 })
+    description: string
+
+    @Column('decimal')
+    price: number
+
+    @ManyToOne(() => Category, products => Product)
+    categoria: Category
+
+}
